8b9c2413c8db66bd04b681797e256d1fdd417eb5
Nick Roberts <djparadyme@gmail.com>
2020-05-01 19:48:58 -0400

    Update script

--- shared/STORJ.old.sh
+++ shared/STORJ.old.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONF=/etc/config/qpkg.conf
-QPKG_NAME="UTLABS"
+QPKG_NAME="STORJ"
 LOGFILE=/var/log/${QPKG_NAME}
 QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
 APACHE_ROOT=`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
@@ -8,33 +8,33 @@ export QNAP_QPKG=$QPKG_NAME
 
 case "$1" in
   start)
-    echo `date` " UTLABS : Request to start ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request to start ($@) " >> $LOGFILE
     ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
     if [ "$ENABLED" != "TRUE" ]; then
         echo "$QPKG_NAME is disabled."
-	echo `date` " UTLABS : $PKG_NAME is disabled " >> $LOGFILE
+	echo `date` " STORJ : $PKG_NAME is disabled " >> $LOGFILE
         exit 1
     fi
     : ADD START ACTIONS HERE
-    echo `date` "Create $QPKG_ROOT/web folder link to UTLABS folder " >> /tmp/runUTLABS.log
-    ln -s $QPKG_ROOT/web.net /home/Qhttpd/Web/UTLABS
-    echo `date` " UTLABS : Request to start ($@) completed " >> $LOGFILE
+    echo `date` "Create $QPKG_ROOT/web folder link to STORJ folder " >> /tmp/runSTORJ.log
+    ln -s $QPKG_ROOT/web /home/Qhttpd/Web/STORJ
+    echo `date` " STORJ : Request to start ($@) completed " >> $LOGFILE
     ;;
 
   stop)
-    echo `date` " UTLABS : Request to stop ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request to stop ($@) " >> $LOGFILE
     : ADD STOP ACTIONS HERE
-    rm /home/Qhttpd/Web/UTLABS
+    rm /home/Qhttpd/Web/STORJ
     ;;
 
   restart)
-    echo `date` " UTLABS : Request restart ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request restart ($@) " >> $LOGFILE
     $0 stop
     $0 start
     ;;
 
   *)
-    echo `date` " UTLABS : Request UNKNOWN ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request UNKNOWN ($@) " >> $LOGFILE
     echo "Usage: $0 {start|stop|restart}"
     exit 1
 esac
