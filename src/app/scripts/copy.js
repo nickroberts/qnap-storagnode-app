b524152423d1011f3f89eb3d8bb04f82fed4466a
Nick Roberts <djparadyme@gmail.com>
2020-05-05 00:25:09 -0400

    Clean build

--- src/app/scripts/copy.js
+++ src/app/scripts/copy.js
@@ -1,12 +1,12 @@
-const fs = require("fs-extra");
-const { resolve } = require("path");
+const fs = require('fs-extra');
+const { resolve } = require('path');
 
 (async () => {
   try {
-    await fs.copy(
-      resolve(__dirname, "../dist/"),
-      resolve(__dirname, "../../../shared/web.new/")
-    );
+    await fs.emptyDir(resolve(__dirname, '../../../shared/web.new/css'));
+    await fs.emptyDir(resolve(__dirname, '../../../shared/web.new/img'));
+    await fs.emptyDir(resolve(__dirname, '../../../shared/web.new/js'));
+    await fs.copy(resolve(__dirname, '../dist/'), resolve(__dirname, '../../../shared/web.new/'));
   } catch (e) {
     console.error(e);
   }
